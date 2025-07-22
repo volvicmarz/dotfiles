58a66d7000f54d762957cf5ecb67e0b3a890655b
volvicmarz <callofduty2006turk@gmail.com>
2025-07-22 17:49:02 +0200

    getopts/trap

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -72,6 +72,7 @@ deleting() {
 
 
 
+trap "echo This script was terminated; exit 1" SIGINT #SIGINT- by CTRL+C, pacti kak interruption request by the user
 menu() {
 echo -ne "
 What do you wanna do now?
@@ -89,3 +90,42 @@ esac
 }
 
 menu
+ 
+#todo with getopts
+
+# trap "echo The script is terminated; exit 1" SIGINT
+
+while getopts ":a:r:sd:hv" opt; do
+	case $opt in 
+		a) 
+			new_task="$OPTARG"
+			tasks+=("$new_task")
+			# echo "${new_task}" >> todo.txt
+			#verbose
+			if [[ $verbose==true ]]; then
+				date=$(date "+%A %B %d")
+				echo "[$date] This task was added: ${new_task}" | tee -a todo.txt
+			fi
+			;;
+		r)
+			to_delete=$((OPTARG +1))
+			sed -i "${to_delete}d" todo.txt
+			;;
+		s)
+			awk 'NR ==1 { print; next } {printf "%d\t%s\n", NR-1,  $0}' todo.txt
+			;;
+		d)
+			already_done="$OPTARG"
+			echo -ne "$(ColourBlue "${tasks[$OPTARG]}") is done "
+			;;
+		v)
+			verbose=true #verbose-provides detailed and extensive amount of information
+			;;
+		\?)
+			echo "Invalid option: -${OPTARG}"
+			;;
+		:)
+			echo "Option -${OPTARG} requires an argument"
+			;;
+	esac
+done
